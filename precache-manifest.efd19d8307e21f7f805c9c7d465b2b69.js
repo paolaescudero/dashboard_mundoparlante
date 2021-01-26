a598a2932a7ac05f2b3e7fc4a54223430e045bc0
violetbaggins <violetbaggins@gmail.com>
2021-01-26 20:01:40 -0300

    Updates

---
+++ precache-manifest.efd19d8307e21f7f805c9c7d465b2b69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72972610a3bb3335ba7c46cadf9f5f5",
+    "url": "/dashboard_mundoparlante/index.html"
+  },
+  {
+    "revision": "07ac27039fdb90bd7541",
+    "url": "/dashboard_mundoparlante/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "4d551b7063706c5c84c8",
+    "url": "/dashboard_mundoparlante/static/js/2.5ad1310c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dashboard_mundoparlante/static/js/2.5ad1310c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ac27039fdb90bd7541",
+    "url": "/dashboard_mundoparlante/static/js/main.5e3f1562.chunk.js"
+  },
+  {
+    "revision": "ee13bff3bb03570b2a44",
+    "url": "/dashboard_mundoparlante/static/js/runtime-main.5cc8d3dd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/dashboard_mundoparlante/static/media/logo.ee7cd8ed.svg"
+  }
+]);
